44998e943be4292a1a9127801de0bdea7782cb8a
dgerth5 <davidgerth5@gmail.com>
2024-08-15 23:15:19 -0500

    not really going anywhere

--- stuff_model.R
+++ stuff_model.R
@@ -2,6 +2,7 @@ library(data.table)
 library(tidyverse)
 library(MLmetrics)
 library(catboost)
+library(ParBayesianOptimization)
 
 df <- fread("C:/Users/david/Downloads/TM_2024_reg_szn (1).csv")
 
@@ -22,8 +23,8 @@ df2 <- df %>%
                                   PitchCall %in% strike_call_events ~ "CalledStrike",
                                   PitchCall %in% strike_sw_events ~ "SwingingStrike",
                                   PitchCall %in% foul_events ~ "FoulBall",
-                                  PitchCall %in% ip_events ~ AutoHitType)) %>%
-  filter(PitchCall %in% c(strike_sw_events, foul_events, ip_events))
+                                  PitchCall %in% ip_events ~ AutoHitType),
+         isSwing = if_else(cleanOutcome %in% c("Ball", "CalledStrike"), 1, 0))
 
 unique(df2$cleanOutcome)
 
@@ -59,3 +60,37 @@ ff_df <- train_test_by_type(df2, bb_type)
 
 train <- as.data.frame(ff_df[[1]])
 test <- as.data.frame(ff_df[[2]])
+
+summary_df <- as.data.frame(train) %>%
+  group_by(cleanOutcome) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  mutate(n2 = n / sum(n),
+         weight = 1/n2)
+
+train$cleanOutcome <- as.integer(as.factor(train$cleanOutcome)) - 1
+test$cleanOutcome <- as.integer(as.factor(test$cleanOutcome)) - 1
+
+train_pool <- catboost.load_pool(data = train[, -1],
+                                 label = train$cleanOutcome)
+test_pool <- catboost.load_pool(data = test[, -1],
+                                label = test$cleanOutcome)
+
+params <- list(
+  loss_function = 'MultiClass',
+  eval_metric = 'AUC',
+  class_weights = summary_df$weight,
+  iterations = 1000,
+  learning_rate = 0.1,
+  depth = 6,
+  random_seed = 134,
+  verbose = 100
+)
+
+model <- catboost.train(train_pool, params = params)
+
+class_predictions <- catboost.predict(model, test_pool, prediction_type = "Class")
+MLmetrics::Accuracy(class_predictions, test$cleanOutcome)
+
+conf_matrix <- table(Predicted = class_predictions, Actual = test$cleanOutcome)
+print(conf_matrix)
