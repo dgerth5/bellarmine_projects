5e7973b5fa027c2af66bb10ab5ecc11ab486a208
dgerth5 <davidgerth5@gmail.com>
2024-08-15 19:19:19 -0500

    need to redo model it's terrible

--- stuff_model.R
+++ stuff_model.R
@@ -5,6 +5,8 @@ library(catboost)
 
 df <- fread("C:/Users/david/Downloads/TM_2024_reg_szn (1).csv")
 
+unique(df$Level)
+
 upc <- sort(unique(df$PitchCall))
 ball_events <- c(upc[2],upc[3])
 strike_call_events <- c(upc[13], upc[14])
@@ -15,15 +17,15 @@ remove_events <- c(upc[1], upc[4], upc[5], upc[17])
 
 df2 <- df %>%
   filter(!PitchCall %in% remove_events) %>%
+  filter(Level == "D1") %>%
   mutate(cleanOutcome = case_when(PitchCall %in% ball_events ~ "Ball",
                                   PitchCall %in% strike_call_events ~ "CalledStrike",
                                   PitchCall %in% strike_sw_events ~ "SwingingStrike",
                                   PitchCall %in% foul_events ~ "FoulBall",
-                                  PitchCall %in% ip_events ~ AutoHitType))
+                                  PitchCall %in% ip_events ~ AutoHitType)) %>%
+  filter(PitchCall %in% c(strike_sw_events, foul_events, ip_events))
 
 unique(df2$cleanOutcome)
-
-df2$PlateLocHeight
 
 apt <- sort(unique(df2$AutoPitchType))
 
@@ -31,7 +33,7 @@ ff_type <- c(apt[4], apt[6])
 bb_type <- c(apt[2], apt[3], apt[7])
 ch_type <- c(apt[1], apt[8])
 
-colnames(df2)
+#colnames(df2)
 
 train_test_by_type <- function(df, pitch_type_vector){
   
@@ -39,10 +41,8 @@ train_test_by_type <- function(df, pitch_type_vector){
   
   df_by_type <- df %>%
     filter(AutoPitchType %in% pitch_type_vector) %>%
-    select(cleanOutcome,RelSpeed,InducedVertBreak,HorzBreak,RelSide,RelHeight,SpinRate,Extension, PitcherThrows) %>%
+    select(cleanOutcome,RelSpeed,InducedVertBreak,HorzBreak,RelSide,RelHeight,SpinRate,Extension) %>%
     drop_na()
-  
-  df_by_type$PitcherThrows <- as.factor(df_by_type$PitcherThrows)
   
   id <- sample(1:nrow(df_by_type), round(0.75*nrow(df_by_type)))
   
@@ -57,49 +57,5 @@ train_test_by_type <- function(df, pitch_type_vector){
 
 ff_df <- train_test_by_type(df2, bb_type)
 
-train_catboost_model <- function(train_df, test_df) {
-  
-  # Convert cleanOutcome to a factor and then to integer
-  train_df$cleanOutcome <- as.integer(as.factor(train_df$cleanOutcome)) - 1
-  test_df$cleanOutcome <- as.integer(as.factor(test_df$cleanOutcome)) - 1
-  
-  # Convert the datasets into CatBoost Pools without specifying text or categorical features
-  train_pool <- catboost.load_pool(data = train_df[, -1], 
-                                   label = train_df$cleanOutcome)
-  test_pool <- catboost.load_pool(data = test_df[, -1], 
-                                  label = test_df$cleanOutcome)
-  
-  # Define the model parameters
-  params <- list(
-    loss_function = 'MultiClass',
-    eval_metric = 'Accuracy',
-    iterations = 1000,
-    learning_rate = 0.1,
-    depth = 6,
-    random_seed = 134,
-    verbose = 100         # Show output every 100 iterations
-  )
-  
-  # Train the model
-  model <- catboost.train(train_pool, params = params)
-  
-  # Predict on the test set
-  predictions <- catboost.predict(model, test_pool, prediction_type = "Probability")
-  
-  # Get the predicted classes
-  predicted_classes <- apply(predictions, 1, which.max) - 1  # CatBoost uses 0-based indexing for classes
-  
-  # Calculate accuracy
-  accuracy <- Accuracy(test_df$cleanOutcome, predicted_classes)
-  
-  # Calculate AUC
-  #auc <- MulticlassAUC(predictions, test_df$cleanOutcome)
-  
-  return(accuracy)
-}
-
-# Train the model and get accuracy and AUC
-results <- train_catboost_model(as.data.frame(ff_df[[1]]), as.data.frame(ff_df[[2]]))
-
-# Print results
-results
+train <- as.data.frame(ff_df[[1]])
+test <- as.data.frame(ff_df[[2]])
