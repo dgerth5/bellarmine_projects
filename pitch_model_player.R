4a70817ea00b43245086c3f3bb4b8c1b01235cea
dgerth5 <davidgerth5@gmail.com>
2024-08-29 09:46:53 -0400

    get stuff scores by pitcher, sanity check

--- pitch_model_player.R
+++ pitch_model_player.R
@@ -29,6 +29,11 @@ ff_type <- c(apt[4], apt[6])
 bb_type <- c(apt[2], apt[3], apt[7])
 ch_type <- c(apt[1], apt[8])
 
+summary(df2$RelSide)
+summary(df2$RelHeight)
+
+df3 <- df2 %>%
+  filter(PitchCall %in% strike_call_events)
 
 model_fn <- function(df, pitch_type_vector){
   
@@ -37,7 +42,7 @@ model_fn <- function(df, pitch_type_vector){
   df_by_type <- df %>%
     filter(AutoPitchType %in% pitch_type_vector) %>%
     mutate(sameHand = if_else(PitcherThrows == BatterSide, 1, 0)) %>%
-    select(cleanOutcome,PlateLocHeight,PlateLocSide,RelSpeed,InducedVertBreak,HorzBreak,RelSide,RelHeight,SpinRate,Extension,sameHand) %>%
+    select(cleanOutcome,RelSpeed,InducedVertBreak,HorzBreak,RelSide,RelHeight,SpinRate,Extension,sameHand) %>%
     drop_na()
   
   df_by_type$sameHand <- as.factor(df_by_type$sameHand)
@@ -71,7 +76,6 @@ ch_model <- model_fn(df2, ch_type)
 
 rvs <- c(.054,-.061,.103,-.039,-.056,.277,-.225,-.108)
 
-
 colnames(ff_preds)
 
 ff_df <- df2 %>%
@@ -85,7 +89,8 @@ ff_pool <- catboost.load_pool(data = ff_df[, -c(1,2,3)])
 ff_preds <- catboost.predict(ff_model, ff_pool, prediction_type='Probability')
 colnames(ff_preds) <- levels(as.factor(df2$cleanOutcome))
 
-ff_df$score <- ff_preds %*% rvs
+ff_df$score <- round(ff_preds %*% rvs,3)
+ff_df$score <- scale(ff_df$score)
 
 bb_df <- df2 %>%
   filter(AutoPitchType %in% bb_type) %>%
@@ -98,7 +103,8 @@ bb_pool <- catboost.load_pool(data = bb_df[, -c(1,2,3)])
 bb_preds <- catboost.predict(bb_model, bb_pool, prediction_type='Probability')
 colnames(bb_preds) <- levels(as.factor(df2$cleanOutcome))
 
-bb_df$score <- bb_preds %*% rvs
+bb_df$score <- round(bb_preds %*% rvs,3)
+bb_df$score <- scale(bb_df$score)
 
 ch_df <- df2 %>%
   filter(AutoPitchType %in% ch_type) %>%
@@ -111,12 +117,11 @@ ch_pool <- catboost.load_pool(data = ch_df[, -c(1,2,3)])
 ch_preds <- catboost.predict(ch_model, ch_pool, prediction_type='Probability')
 colnames(ch_preds) <- levels(as.factor(df2$cleanOutcome))
 
-ch_df$score <- ch_preds %*% rvs
+ch_df$score <- round(ch_preds %*% rvs,3)
+ch_df$score <- scale(ch_df$score)
 
 pitch_score_df <- bind_rows(ff_df, bb_df, ch_df) %>%
   group_by(Pitcher, AutoPitchType) %>%
-  summarise(pitch_score = 100 + (round(mean(score),3) / .027 * -10),
+  summarise(pitch_score = round(100 + (mean(score) * -10),4),
             n = n()) %>%
   filter(n > 30)
-
-mean(pitch_score_df$n)
